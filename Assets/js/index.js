3f50d15c270a1323ab38117040a8910b4f4502e7
Juan Parra <parrajuan1025@gmail.com>
2024-05-06 04:46:37 -0400

    MVP ready

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,4 +1,5 @@
 const searchbar = document.getElementById('searchbar')
+const searchlist = document.getElementById('list')
 const searchbox = document.getElementById('searchbox')
 const searchbtn = document.getElementById('searchBtn')
 const cityInfo = document.getElementById('cityInfo')
@@ -6,45 +7,49 @@ const weatherinfo = document.getElementById('currentConditions')
 const forecast = document.getElementById('forecast')
 const APIKey = '1f84a10cb7da5db222b9d855961dbfd7'
 
+
 const cities = []
 const weatherhistory = []
 const forecasthistory = []
 const currentwind = []
 const icon = []
-//take input from search box and store it into city
 
+
+//take input from search box, pull and organize data from API.
 async function getCity (city) {
-    // let currentcity = searchbox.value
     if (!city) {
     }
     else {
     const weatherApi = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`)
     const weatherdata = await weatherApi.json()
-    // currentConditions.push(weatherdata.main);
     const forecastApi = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`)
     const forecastdata = await forecastApi.json()
+ //current weather conditions and sent up to global storage
     let cityweather = weatherdata.main
     let weathericon = weatherdata.weather[0].icon
-    icon.push(weathericon)
-    
     let windspeed = weatherdata.wind
+// pushed variables up to lcoal storage
+    icon.push(weathericon)  
     currentwind.push(windspeed)
-    let cityforecast = forecastdata.list
-    cities.push(city)
     weatherhistory.push(cityweather)
+    cities.push(city)
+// built an array for a 5 day forecast and sent up to global storage
+    let cityforecast = forecastdata.list
      let fivedayforecast = [
-        cityforecast[0],
-        cityforecast[8],
-        cityforecast[16],
-        cityforecast[24],
-        cityforecast[32]
+        cityforecast[7],
+        cityforecast[15],
+        cityforecast[23],
+        cityforecast[31],
+        cityforecast[39]
      ]
      forecasthistory.push(...fivedayforecast)
     }
 } 
 
-
+//set variable to track 
 let n = 0
+
+//function for placing data into objects
 
 async function postdata () { 
     cityInfo.innerHTML = '';
@@ -53,13 +58,23 @@ async function postdata () {
             let currentcity = cities[i];
             let currentconditions = weatherhistory[i];
             let windspeed = currentwind[i].speed
-            //let currentforecast = forecasthistory[i]
+
+            let history = document.createElement('div')
+            history.setAttribute('class','history')
+            for (h=0;h<cities.length;h++) {
+                let cityname = document.createElement('button')
+                cityname.setAttribute('class',`${cities[h]}`)
+                cityname.innerText = `${cities[h]}`
+                history.appendChild(cityname)
+            }
+            searchlist.appendChild(history)
             
             let main = document.createElement('div')
             main.setAttribute('class','cityinfo')
             
             let cityname = document.createElement('h1')
             cityname.innerText = currentcity
+           
             main.appendChild(cityname)
            
             let cityinfo = document.createElement('div')
@@ -85,12 +100,12 @@ async function postdata () {
             cityInfo.appendChild(main)
 
             let fiveday = document.createElement('div')
-            fiveday.innerText = 'FIVE DAY FORECAST'
+            fiveday.setAttribute('class','fiveday')
             
             
             for (d=n; d<forecasthistory.length;d++) {
                 let day = document.createElement('div')
-
+                day.setAttribute('class','date')
                 let iconlocation = forecasthistory[d].weather[0].icon
                 let iconspot = document.createElement('img')
                 iconspot.setAttribute('src',`https://openweathermap.org/img/wn/${iconlocation}@2x.png`)
@@ -98,19 +113,19 @@ async function postdata () {
 
 
                 let temp = forecasthistory[d].main.temp
-                let forecasttemp = document.createElement('h3')
-                forecasttemp.innerText = `Temp: ${temp} degrees farenheight`
+                let forecasttemp = document.createElement('p')
+                forecasttemp.innerText = `Temp: ${temp} degrees`
                 day.appendChild(forecasttemp)
-                let feelslike = document.createElement('h3')
-                feelslike.innerText = `Feels like: ${forecasthistory[d].main.feels_like} degrees farenheight`
+                let feelslike = document.createElement('p')
+                feelslike.innerText = `Feels like: ${forecasthistory[d].main.feels_like} degrees`
                 day.appendChild(feelslike)
-                let forecasthumidty = document.createElement('h3')
-                forecasthumidty.innerText = `Humidity: ${forecasthistory[d].main.humidity} % humidity`
+                let forecasthumidty = document.createElement('p')
+                forecasthumidty.innerText = `Humidity: ${forecasthistory[d].main.humidity} %`
                 day.appendChild(forecasthumidty)
-                let forecastwind = document.createElement('h3')
+                let forecastwind = document.createElement('p')
                 forecastwind.innerText = `WindSpeed: ${forecasthistory[d].wind.speed} MPH`
                 day.appendChild(forecastwind)
-                let date = document.createElement('h1')
+                let date = document.createElement('p')
                 date.innerText = `Date: ${forecasthistory[d].dt_txt}`
                 day.appendChild(date)
                 fiveday.appendChild(day)
@@ -123,11 +138,22 @@ async function postdata () {
 
 searchbtn.addEventListener('click',(event => {
     event.preventDefault()
-
+    searchlist.innerHTML = ''
     forecast.innerHTML = ''
     postdata()
     searchbox.value = ''
 }))
+
+searchlist.addEventListener('click',event => {
+    searchlist.innerHTML = ''
+    forecast.innerHTML = ''
+    let buttons = event.target
+    let inputcity = buttons.innerText
+    searchbox.value = `${inputcity}`
+    console.log(inputcity)
+    postdata()
+    searchbox.value = ''
+})
 
 //         forecasthistory.forEach(() => {
 //             let fiveday = document.createElement('div')
